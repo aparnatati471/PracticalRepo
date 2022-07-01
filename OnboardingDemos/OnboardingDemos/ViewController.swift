6aca7c2a6167557352cfb872c17487c102b32d66
AparnaTati <aparna.t@simformsolutions.com>
2022-07-01 11:12:28 +0530

    ADD: Project Setup

---
+++ OnboardingDemos/OnboardingDemos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnboardingDemos
+//
+//  Created by Aparna Tati on 01/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
